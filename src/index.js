c7921d72f0b9a8949a0dfbf54d80b6690c3665f5
Jillian Werner <jillianlwerner@gmail.com>
2021-02-04 16:42:41 -0500

    Add basic task dropdown and mark-as-complete button

--- src/index.js
+++ src/index.js
@@ -2,4 +2,33 @@ import './style.scss'
 // Uncomment to enable masonry layout
 // FlexMasonry.init('.projects-list');
 
-console.log("Testing webpack");
+const projectTasks = document.querySelectorAll('.project-task');
+const projectTasksComplete = document.querySelectorAll('.details-complete');
+
+projectTasks.forEach(function(task) {
+  task.addEventListener('click', function(e) {
+    e.stopPropagation();
+    openSubMenu(e.target);
+  }), true});
+
+projectTasksComplete.forEach(function(task) {
+  task.addEventListener('click', markAsComplete);
+});
+
+function openSubMenu(project) {
+  let sub = project.querySelector('.project-task-details');
+  if (sub != null) {
+    sub.classList.toggle('hidden');
+  };
+}
+
+function markAsComplete() {
+  let task = this.closest('.project-task');
+  task.classList.toggle('checkedoff');
+
+  if (task.classList.contains('checkedoff')) {
+    this.innerText = "Finished!";
+  } else {
+    this.innerText = "Mark as finished";
+  };
+}
