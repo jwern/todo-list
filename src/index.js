22ec7f7cb7aeef6243a4a3c237de7de981e929d7
Jillian Werner <jillianlwerner@gmail.com>
2021-02-05 13:01:24 -0500

    Create add task button

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import './style.scss'
 const projectTasks = document.querySelectorAll('.project-task');
 const projectTasksComplete = document.querySelectorAll('.details-complete');
 
+// eventListener will need to be added every time a new task is created
 projectTasks.forEach(function(task) {
   task.addEventListener('click', function(e) {
     e.stopPropagation();
     openSubMenu(e.target);
   }), true});
 
+// add to each new task
 projectTasksComplete.forEach(function(task) {
   task.addEventListener('click', markAsComplete);
 });
 
+// add to each new task
 function openSubMenu(project) {
   let sub = project.querySelector('.project-task-details');
   if (sub != null) {
@@ -22,6 +25,7 @@ function openSubMenu(project) {
   };
 }
 
+// add to each new task
 function markAsComplete() {
   let task = this.closest('.project-task');
   task.classList.toggle('checkedoff');
